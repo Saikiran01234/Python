32a25d8e89b91564753f47ab500dc0a775c07892
Saikiran <josyulasai.03@gmail.com>
2025-06-27 11:04:07 +0530

    First Commit

---
+++ day8.py
@@ -0,0 +1,19 @@
+class MyClass:
+    def add(self):
+        print("addition of two numbers")
+        return 2 + 3
+    def sub(self):
+        print("subtraction of two numbers")
+        return 5 - 3
+    def mul(self):
+        print("multiplication of two numbers")
+        return 2 * 3
+    def div(self):
+        print("division of two numbers")
+        return 6 / 3
+x = MyClass()
+print(x.add())
+print(x.sub())
+print(x.mul())
+print(x.div())
+
